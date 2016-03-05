56ee3f56d7f9c489a6df93cca26079f0d2e97631
vitalya kvas <vitalya kvas>
2016-03-05 18:49:01 +0200

    Added helper SearchOption.

---
+++ MonkeyTests/MonkeyHelper/Code/SearchOptionModel.tstest.cs
@@ -0,0 +1,25 @@
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace MonkeyTests
+{
+    public enum SearchOptionModel
+    {
+        ByXPath = 1,
+    }
+}
