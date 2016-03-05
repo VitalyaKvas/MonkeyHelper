87aa40e2a48e578a6a565bd9af6ea88561be1212
vitalya kvas <vitalya kvas>
2016-03-05 19:25:39 +0200

    Change Test #0, replaced step for click on login button.

---
+++ MonkeyTests/TestsForWebDemoProject/Test #0 - Attempt login with invalid credentials, verify correct error message.tstest.cs
@@ -0,0 +1,62 @@
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace MonkeyTests
+{
+
+    public class Test__0___Attempt_login_with_invalid_credentials__verify_correct_error_message : BaseWebAiiTest
+    {
+        #region [ Dynamic Pages Reference ]
+
+        private Pages _pages;
+
+        /// <summary>
+        /// Gets the Pages object that has references
+        /// to all the elements, frames or regions
+        /// in this project.
+        /// </summary>
+        public Pages Pages
+        {
+            get
+            {
+                if (_pages == null)
+                {
+                    _pages = new Pages(Manager.Current);
+                }
+                return _pages;
+            }
+        }
+
+        #endregion
+        
+        // Add your test methods here...
+    
+        [CodedStep(@"Click on login button")]
+        public void Test_0__Attempt_login_with_invalid_credentials_verify_correct_error_message_CodedStep()
+        {
+            var useMouse = new ClickOnElementModel()
+            {
+                SearchOption = SearchOptionModel.ByXPath,
+                Selection = ".//*[@id='login_button']",
+            };
+            SetExtractedValue("ClickOnElementModel", useMouse);
+            
+            ExecuteTest(Constans.ClickOnElement);
+        }
+    }
+}
