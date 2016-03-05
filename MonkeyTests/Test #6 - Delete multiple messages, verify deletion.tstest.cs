7a213b31afb13391f794548fdbc8b68389fd3e1d
vitalya kvas <vitalya kvas>
2016-03-05 14:51:40 +0200

    First commit. Demo project.

---
+++ MonkeyTests/Test #6 - Delete multiple messages, verify deletion.tstest.cs
@@ -0,0 +1,95 @@
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace new_test_project
+{
+
+    //
+    // You can add custom execution steps by simply
+    // adding a void function and decorating it with the [CodedStep] 
+    // attribute to the test method. 
+    // Those steps will automatically show up in the test steps on save.
+    //
+    // The BaseWebAiiTest exposes all key objects that you can use
+    // to access the current testcase context. [i.e. ActiveBrowser, Find ..etc]
+    //
+    // Data driven tests can use the Data[columnIndex] or Data["columnName"] 
+    // to access data for a specific data iteration.
+    //
+    // Example:
+    //
+    // [CodedStep("MyCustom Step Description")]
+    // public void MyCustomStep()
+    // {
+    //        // Custom code goes here
+    //      ActiveBrowser.NavigateTo("http://www.google.com");
+    //
+    //        // Or
+    //        ActiveBrowser.NavigateTo(Data["url"]);
+    // }
+    //
+        
+
+    public class Test__6___Delete_multiple_messages__verify_deletion : BaseWebAiiTest
+    {
+        #region [ Dynamic Pages Reference ]
+
+        private Pages _pages;
+
+        /// <summary>
+        /// Gets the Pages object that has references
+        /// to all the elements, frames or regions
+        /// in this project.
+        /// </summary>
+        public Pages Pages
+        {
+            get
+            {
+                if (_pages == null)
+                {
+                    _pages = new Pages(Manager.Current);
+                }
+                return _pages;
+            }
+        }
+
+        #endregion
+        
+        // Add your test methods here...
+    
+        [CodedStep(@"New Coded Step")]
+        public void WebTest3_CodedStep()
+        {
+            foreach (HtmlTableRow r in Pages.TelerikSample0.InboxTable.Rows)
+            {
+                HtmlTableCell c = r.Cells[2]; // the date will always be in the third cell
+            
+                if (c.TextContent.Contains("Wed"))
+                {
+                    HtmlTableCell c1 = r.Cells[0]; // look at the first cell in the row
+                    HtmlControl checkb = c1.ChildNodes[0].As<HtmlControl>(); // find the checkbox within the cell
+                    checkb.ScrollToVisible(); //Make sure we see the object before we click it
+                    checkb.MouseClick();  // MouseClick rather than Click so any existing events will be fired
+                }
+            }
+            
+            
+            
+        }
+    }
+}
