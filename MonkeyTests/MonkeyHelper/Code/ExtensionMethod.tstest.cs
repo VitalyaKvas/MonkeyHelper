b1cbbb5e37b5dd880106ca7b5710a9156299cdf9
vitalya kvas <vitalya kvas>
2016-03-05 18:50:45 +0200

    Added extension methods: GetExtractedValue<T>, TryGetExtractedValue<T>.

--- MonkeyTests/MonkeyHelper/Code/ExtensionMethod.tstest.cs
+++ MonkeyTests/MonkeyHelper/Code/ExtensionMethod.tstest.cs
@@ -47,5 +47,45 @@ namespace MonkeyTests
                     log.WriteLineInLogAndConsole(ex.ToString());
             }
         }
+        
+        /// <summary>
+        /// GetExtractedValue<T>
+		/// It is a versatile technique that is used for simplified type conversion.
+		/// </summary>
+        public static T GetExtractedValue<T>(this BaseWebAiiTest webAii, string dataBindVariableName) 
+            where T : class
+        {
+            var value = (T)webAii.GetExtractedValue(dataBindVariableName);
+            return value;
+        }
+        
+        /// <summary>
+        /// TryGetExtractedValue<T>
+        /// This method is a wrapper over the method GetExtractedValue.
+        /// It catch all internal exception in method GetExtractedValue.
+		/// </summary>
+        public static T TryGetExtractedValue<T>(this BaseWebAiiTest webAii, string dataBindVariableName, Log log = null) 
+            where T : class
+        {
+            T value = null;
+            
+            try
+            {
+                value = webAii.GetExtractedValue<T>(dataBindVariableName);
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format("Exception in GetExtractedValue : {0}   DataBindVariableName - {1} {0}   Message - {2} {0}   InnerException - {3} {0}", 
+                                            Environment.NewLine,
+                                            dataBindVariableName,
+                                            ex.Message,
+                                            ex.InnerException);
+                
+                if (log != null)
+                    log.WriteLineInLogAndConsole(message);
+            }
+            
+            return value;
+        }
     }
 }
