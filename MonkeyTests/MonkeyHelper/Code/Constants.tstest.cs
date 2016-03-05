6a5c2ea7a54492fd84ec336faee7309e4468dc8a
vitalya kvas <vitalya kvas>
2016-03-05 21:31:00 +0200

    Added in Contants, field for path for helper EnterText.

--- MonkeyTests/MonkeyHelper/Code/Constants.tstest.cs
+++ MonkeyTests/MonkeyHelper/Code/Constants.tstest.cs
@@ -23,5 +23,6 @@ namespace MonkeyTests
         public static string BrowserWaitUntilReady = @"MonkeyTests\MonkeyHelper\Steps\MonkeyHelper_BrowserWaitUntilReady.tstest";
         public static string NavigationToBaseUrl = @"MonkeyTests\MonkeyHelper\Steps\MonkeyHelper_NavigationToBaseUrl.tstest";
         public static string ClickOnElement = @"MonkeyTests\MonkeyHelper\Steps\MonkeyHelper_ClickOnElement.tstest";
+        public static string EnterText = @"MonkeyTests\MonkeyHelper\Steps\MonkeyHelper_EnterText.tstest";
     }
 }
