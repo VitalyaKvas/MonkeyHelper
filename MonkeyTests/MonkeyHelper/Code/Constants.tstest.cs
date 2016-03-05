b1eb8843469a05620d1640d5360b8a329a58d0b3
vitalya kvas <vitalya kvas>
2016-03-05 19:00:24 +0200

    Added class for Constants.

---
+++ MonkeyTests/MonkeyHelper/Code/Constants.tstest.cs
@@ -0,0 +1,27 @@
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace MonkeyTests
+{
+    public static class Constans
+    {
+        public static string BrowserWaitUntilReady = @"MonkeyHelper\Steps\BrowserWaitUntilReady.tstest";
+        public static string NavigationToBaseUrl = @"MonkeyHelper\Steps\NavigationToBaseUrl.tstest";
+        public static string ClickOnElement = @"MonkeyHelper\Steps\ClickOnElement.tstest";
+    }
+}
